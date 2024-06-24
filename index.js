a16fd379dcf9f0b2ea61320ee287bd852d950de8
OlimovN <olimonurmuhammad711@gmail.com>
2024-06-24 11:21:36 +0500

    add class

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function changeStyle(button) {
+function myBtn(button) {
   let blocks = document.querySelectorAll(".block");
   blocks.forEach((block) => {
     block.className = "block " + button;
